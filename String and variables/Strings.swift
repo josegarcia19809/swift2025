f7683d31cfee0401fb770751bec5f2a17c56cb6b
Jose Garcia <josegarcia19809@gmail.com>
2024-12-29 13:40:33 -0600

    String functions II

--- String and variables/Strings.swift
+++ String and variables/Strings.swift
@@ -31,3 +31,33 @@ func lowerUpperCase()->String{
 func multilinea()->String{
     return "El trabajo va a ocupar la mayor parte de tu vida, \ny la única forma de estar realmente satisfecho\n es hacer aquello que crees es un gran trabajo."
 }
+
+func trimString()->String{
+    var cita = "       El trabajo va \n\na ocupar la mayor parte de tu \tvida          "
+    return cita.trimmingCharacters(in: .whitespacesAndNewlines)
+}
+
+func estaVacia(texto: String)->Bool{
+    return texto.isEmpty
+}
+
+func esMensajeVacio()->String{
+    let mensaje = "There is a beatiful life"
+    return mensaje + "(vacio?): " + String(estaVacia(texto: mensaje))
+}
+
+func contieneSubcadena()->String{
+    let mensaje = "I will not let myself be controlled by anything"
+    return mensaje + " contiene 'myself': " + String(mensaje.contains("myself"))
+}
+
+func primeraUltimaLetras()->String{
+    let lenguaje = "swift"
+    return lenguaje + ", primera letra: " + String(lenguaje.first!) + ", última letra: "
+    + String(lenguaje.last!)
+}
+
+func obtenerElementoRandom()->String{
+    let lenguaje = "kotlin"
+    return lenguaje + ": " + String( lenguaje.randomElement()!)
+}
